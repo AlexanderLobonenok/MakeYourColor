43c3f57a547877b146997182020c558a9725c131
AlexanderLobonenok <110338796+AlexanderLobonenok@users.noreply.github.com>
2022-09-05 14:32:02 +0300

    New project created

---
+++ MakeYourColorApp/MakeYourColorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MakeYourColorApp
+//
+//  Created by Alexander Lobonenok on 5.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
